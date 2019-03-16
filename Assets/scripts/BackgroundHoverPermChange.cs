925bfe9a5761625cf6f32e7bd5f21e019fd514d1
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-16 15:07:40 -0400

    backgroundchange

---
+++ Assets/scripts/BackgroundHoverPermChange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundHoverPermChange : MonoBehaviour
+{
+    public GameObject originalBackground;
+    public GameObject newBackGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseEnter()
+    {
+       originalBackground.SetActive(false);
+        newBackGround.SetActive(true);
+
+       
+
+    }
+}
