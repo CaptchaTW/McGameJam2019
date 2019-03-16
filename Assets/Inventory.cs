439f4f4a5781243a34a5cfb24fa3b63afed18115
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-15 22:50:10 -0400

    scripts

---
+++ Assets/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory InventoryKeys;
+    public bool[] inventoryOfKeys = new bool[5];
+    void Awake()
+    {
+        if (InventoryKeys != null)
+            GameObject.Destroy(InventoryKeys);
+        else
+            InventoryKeys = this;
+
+        DontDestroyOnLoad(this);
+    }
+}
---
+++ Assets/KeyClick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyClick: MonoBehaviour
+{
+    GameObject Player;
+    public GameObject Key;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+    void OnMouseDown()
+    {
+        Player = GameObject.Find("Player");
+        Inventory.InventoryKeys.inventoryOfKeys[0] = true ;
+        Key.gameObject.SetActive(false);
+
+    }
+
+}
