753a8bfd115f86a3b1214fd7d350c8e6d1daecfa
Canofpencils <37453492+Canofpencils@users.noreply.github.com>
2019-03-16 11:45:15 -0400

    Revert "Merge branch 'master' of https://github.com/CaptchaTW/McGameJam2019"
    This reverts commit da8a9adb2e60a64e84ca5567db97e8208dcdf23e, reversing
    changes made to db8502784b4055097fc7446f7e727fdbbca49a97.

---
+++ Assets/MouseFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollow : MonoBehaviour
+    
+{
+    float speed = 8f;
+    // Start is called before the first frame update
+    public Transform lockerbg;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.mousePosition.x < Screen.width / 4)
+        {
+            
+            transform.position += new Vector3(Time.deltaTime * speed, transform.position.y, transform.position.z);
+        }
+        else if (Input.mousePosition.x > (3*Screen.width) / 4) 
+        {
+            transform.position -= new Vector3(Time.deltaTime * speed, transform.position.y, transform.position.z);
+        }
+    }
+}
