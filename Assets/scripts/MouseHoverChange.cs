7a45202520613e5324ce64b8ffac6cdd7358c138
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-16 11:18:25 -0400

    mouseHoverScript

---
+++ Assets/scripts/MouseHoverChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHoverChange : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D cursor;
+
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+}
