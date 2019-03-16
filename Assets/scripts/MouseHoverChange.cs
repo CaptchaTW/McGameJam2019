c7e95790b404157c63b9a00d89771ce0a73c79a7
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-16 11:26:41 -0400

    mouseHoverposition

--- Assets/scripts/MouseHoverChange.cs
+++ Assets/scripts/MouseHoverChange.cs
@@ -6,16 +6,16 @@ public class MouseHoverChange : MonoBehaviour
 {
     [SerializeField]
     Texture2D cursor;
-
+    Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
     void OnMouseEnter()
     {
-        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(cursor, mousePosition, CursorMode.Auto);
     }
 
     void OnMouseExit()
     {
-        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(null, mousePosition, CursorMode.Auto);
     }
 
 }
