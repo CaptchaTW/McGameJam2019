55155c04695215241037c053fd5f1bf3cbf8f87d
Ai Di Wang <aidiwang77@gmail.com>
2019-03-15 23:12:34 -0400

    camera mouse follow

--- Assets/MouseFollow.cs
+++ Assets/MouseFollow.cs
@@ -8,22 +8,35 @@ public class MouseFollow : MonoBehaviour
     float speed = 8f;
     // Start is called before the first frame update
     public Transform lockerbg;
+    Vector3 posBounds;
+
     void Start()
     {
-        
+        posBounds = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.mousePosition.x < Screen.width / 4)
+        if (Input.mousePosition.x < Screen.width / 4 && transform.position.x < 9)
         {
-            
-            transform.position += new Vector3(Time.deltaTime * speed, transform.position.y, transform.position.z);
+
+            Debug.Log(transform.position.x);
+
+            posBounds = new Vector3(posBounds.x + Time.deltaTime * speed, posBounds.y, transform.position.z);
+           
         }
-        else if (Input.mousePosition.x > (3*Screen.width) / 4) 
+        else if (Input.mousePosition.x > (3 * Screen.width) / 4 && transform.position.x > -73)
         {
-            transform.position -= new Vector3(Time.deltaTime * speed, transform.position.y, transform.position.z);
+            Debug.Log(transform.position.x);
+            //transform.position -= new Vector3(Time.deltaTime * speed, transform.position.y, transform.position.z);
+
+            posBounds = new Vector3(posBounds.x - Time.deltaTime * speed, posBounds.y, transform.position.z);
+
         }
+        
+
+        transform.position = posBounds + new Vector3(Random.Range(-0.02f, 0.02f), Random.Range(-0.02f, 0.02f), Random.Range(-0.02f, 0.02f));
     }
+
 }
