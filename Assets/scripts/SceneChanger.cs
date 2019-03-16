b599f384db8a7fd24cdfcaae4afc856796a0e14a
Ai Di Wang <aidiwang77@gmail.com>
2019-03-16 17:11:50 -0400

    added transition sounds

--- Assets/scripts/SceneChanger.cs
+++ Assets/scripts/SceneChanger.cs
@@ -4,36 +4,74 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
+    bool wait = false;
+
+
     public string SceneName;
     public float x;
     public int key;
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    IEnumerator Pause()
+    {
+        Debug.Log("enumerator");
+        wait = true;
+        yield return new WaitForSeconds(0.7f);
+        Debug.Log("waited");
+        wait = false;
+        SceneManager.LoadScene(SceneName);
     }
 
     void OnMouseDown()
     {
-        if (gameObject.tag == "Locked" && GlobalVars.inventoryOfKeys[key] == true){
+        if (gameObject.tag == "Locked" && GlobalVars.inventoryOfKeys[key] == true)
+        {
             gameObject.tag = "Unlocked";
             Debug.Log("Unlocked with key" + key);
         }
-        else if (gameObject.tag != "Locked"){
-            SceneManager.LoadScene(SceneName);
-            Debug.Log("Clicked");
+        else if (gameObject.tag != "Locked")
+        {
+            if (SceneName.Equals("MusicRoom") || SceneName.Equals("Classroom") || SceneName.Equals("Library") || SceneName.Equals("Bathroom"))
+            {
+                SoundScript.PlaySound("doorOpen");
+
+                if (!wait)
+                {
+                    StartCoroutine(Pause());
+                }
+            }
+            else if (SceneName.Equals("Rooftop") || SceneName.Equals("StairsDown"))
+            {
+                SoundScript.PlaySound("footsteps");
+
+                if (!wait)
+                {
+                    StartCoroutine(Pause());
+                }
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneName);
+                Debug.Log("Clicked");
+            }
             GlobalVars.cameraX = x;
         }
-        else {
+        else
+        {
             Debug.Log("key is " + key + " and " + GlobalVars.inventoryOfKeys[key]);
             // Debug.Log("Locked");
         }
 
         // GameObject.Find("Main Camera").transform.position = new Vector3(x, 0, 0);
     }
+
 }
