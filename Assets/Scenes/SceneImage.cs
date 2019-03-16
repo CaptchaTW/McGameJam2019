219e24029115d1aab19cba73276661a6002c8536
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-15 20:30:15 -0400

    initial commit

---
+++ Assets/Scenes/SceneImage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+public class SceneImage : MonoBehaviour
+{
+    public Texture atexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void onGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 10, 10), atexture);
+    }
+}
