d99f86b1936fd304cf5613426e99d9303510d32d
CaptchaTW <32964205+CaptchaTW@users.noreply.github.com>
2019-03-17 14:28:09 -0400

    return after x seconds if not in classroom or rooftop

--- Assets/scripts/RandomSfxScript.cs
+++ Assets/scripts/RandomSfxScript.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class RandomSfxScript : MonoBehaviour
-{
+{ float timer=0;
     int clipIndex; 
 
     void Start()
     {
         StartCoroutine(PlaySound());
     }
-
+    void Update()
+    {
+        timer += Time.deltaTime;
+    }
+        
     IEnumerator PlaySound()
     {
-        if (SceneManager.GetActiveScene().name.Equals("Main") ||
-            SceneManager.GetActiveScene().name.Equals("Schoolyard"))
+           if (SceneManager.GetActiveScene().name.Equals("Main") ||
+             SceneManager.GetActiveScene().name.Equals("Schoolyard"))
         {
-            Debug.Log("no sound");
-            yield return new WaitForSeconds(5f);
-            StartCoroutine(PlaySound());
+          Debug.Log("no sound");
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(PlaySound());
         }
         else
         {
@@ -31,11 +35,23 @@ public class RandomSfxScript : MonoBehaviour
             SoundScript.PlaySound(SoundScript.clips[clipIndex]);
 
             yield return new WaitForSeconds(Random.Range(15f, 20f));
-            
 
+            timer = 0;
             Debug.Log("play bell sound");
             SoundScript.PlaySound("bellsound");
+            yield return new WaitForSeconds(11f);
 
+            for (int x = 1; x < 6; x++)
+            {
+                GlobalVars.inventoryOfKeys[x] = false;
+                GlobalVars.keyStatus[x] = false;
+            }
+            if (SceneManager.GetActiveScene().name == "Classroom" || SceneManager.GetActiveScene().name == "Rooftop") { }
+            else
+            {
+                SceneManager.LoadScene("Main");
+            }
+       
 
             StartCoroutine(PlaySound());
         }
